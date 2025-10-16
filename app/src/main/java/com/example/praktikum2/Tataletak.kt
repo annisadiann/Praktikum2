19f00ba0e3a747d94391712beda3015c26e4b649
annisadiann <annisadian0412@gmail.com>
2025-10-16 17:11:53 +0700

    Menambahkan komponen TataletakBoxColumnRow dengan dua Box berwarna dan susunan teks

--- app/src/main/java/com/example/praktikum2/Tataletak.kt
+++ app/src/main/java/com/example/praktikum2/Tataletak.kt
@@ -127,6 +127,14 @@ fun TataletakBoxColumnRow(modifier: Modifier) {
                 }
             }
         }
+        Spacer(modifier = Modifier.height(height = 10.dp))
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height = 300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        ){
 
 
 
